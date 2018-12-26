83c679720e5e4dd32351846a63f306b59ef2a020
thundersen <thundersen@posteo.de>
2018-12-26 13:35:13 +0100

    fixed zero daughter script

--- zero_daughters.py
+++ zero_daughters.py
@@ -2,9 +2,9 @@ from datetime import datetime
 
 from logreader.reader import read_characters
 
-LOG = 'test_data/server01/2018_12December_18_Tuesday.txt'
+LOG = 'lifelogs/server01/2018_12December_18_Tuesday.txt'
 
-NAMES = 'test_data/server01/2018_12December_18_Tuesday_names.txt'
+NAMES = 'lifelogs/server01/2018_12December_18_Tuesday_names.txt'
 
 START = datetime(2018, 12, 18)
 
@@ -12,13 +12,12 @@ END = datetime(2018, 12, 19)
 
 
 if __name__ == '__main__':
-    characters = read_characters(NAMES, LOG)
+    history = read_characters(NAMES, LOG)
 
-    for c in characters.values():
-        c.read_kids(characters)
+    zero_daughters = [x for x in history.all_characters() if x.is_zero_girl_mom()]
 
-    zero_daughters = [x for x in characters.values() if x.is_zero_girl_mom()]
+    with_daughters = [x for x in history.all_characters() if x.is_mom_with_girls()]
 
-    with_daughters = [x for x in characters.values() if x.is_mom_with_girls()]
+    survivor_mums = [x for x in history.all_characters() if x.is_surviving_mom()]
 
     print(len(zero_daughters))
