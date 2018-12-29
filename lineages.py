185db11a9030afc6fc5e23883ab78abe0181fce8
thundersen <thundersen@posteo.de>
2018-12-29 23:07:09 +0100

    filter lineages for plot with min length of 1h

--- lineages.py
+++ lineages.py
@@ -10,16 +10,20 @@ SERVERS = [1, 2, 3]
 FROM_DATE = date(2018, 11, 1)
 TO_DATE = date(2018, 12, 28)
 
+MIN_LINEAGE_LENGTH_SECONDS = 3600
+
 
 def to_hours(duration):
-    delta = (duration.end_datetime - duration.start_datetime)
-
-    return delta.days * 24 + float(delta.seconds) / 3600
+    return float(duration.get_timedelta_second()) / 3600
 
 
-def make_lineage_plot(history):
-    lineages = history.all_lineages()
-    matrix = [[l.id(), l.duration().end_datetime, to_hours(l.duration())] for l in lineages]
+def make_lineage_plot(lineages):
+
+    filtered_lineages = [l for l in lineages if l.duration().get_timedelta_second() >= MIN_LINEAGE_LENGTH_SECONDS]
+
+    print(f'plotting {len(filtered_lineages)} lineages')
+
+    matrix = [[l.id(), l.duration().end_datetime, to_hours(l.duration())] for l in filtered_lineages]
     df = pd.DataFrame.from_records(matrix, index='id', columns=['id', 'death', 'lifespan_hours'])
     return go.Scatter(x=df['death'], y=df['lifespan_hours'], mode='markers', name='ending lineages')
 
@@ -28,7 +32,7 @@ if __name__ == '__main__':
 
     history = read_characters(SERVERS, FROM_DATE, TO_DATE)
 
-    lineage_plot = make_lineage_plot(history)
+    lineage_plot = make_lineage_plot(history.all_lineages())
 
     layout = dict(
         title='Lineages on Servers %s' % (", ".join(str(s) for s in SERVERS)),
